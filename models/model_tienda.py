ad3131105fc347b8cefeac9097e5c128cd9b8cd7
Victor Avila <victoravila@Mac-mini-de-Victor.local>
2023-05-18 11:53:35 -0600

    CRUD Tienda completado

--- models/model_tienda.py
+++ models/model_tienda.py
@@ -11,20 +11,16 @@ def leer_tiendas():
     resultado = q.consulta_lista(resultado)
     return resultado
 def leer_tienda(id):
-    resultado = q.select("Select * from almacen where id ="+str(id)+"AND status=1")
+    consulta = "Select * from tienda where id = "+str(id)+" AND estatus = 1"
+    resultado = q.select(consulta)
     resultado = q.consulta_lista(resultado)
     return resultado[0]
 def editar_tienda(id,valores):
     #esto sirve para agregar el id al final de la lista ya que asi es la sintaxis para agregar valores a la consulta
+    valores.append(q.timestamp())
     valores.append(id)
     resultado = q.mod("Update tienda set nombre = %s, direccion = %s, updated_at = %s where id = %s",(valores[0],valores[1],valores[2],valores[3]))
     return resultado
 def eliminar_tienda(id):
     resultado = q.mod("Update tienda set estatus = %s where id = %s",(2,id))
     return resultado
-
-resultado = eliminar_tienda(1)
-if(resultado == True):
-    print("Se ha eliminado con exito la tienda")
-else:
-    print("Ha ocurrido un error al eliminar la tienda")
--- views/view_tienda.py
+++ views/view_tienda.py
@@ -23,16 +23,57 @@ def mostrar_tiendas():
     tiendas = tienda.leer_tiendas()
     print("ID\tNombre\tDireccion\tFecha\n")
     for row in tiendas:
-        print(row)
+        print(str(row[0])+"\t"+str(row[1])+"\t"+str(row[2])+"\t"+str(row[4]))
+def editar_tienda(id):
+    valor_viejo = tienda.leer_tienda(id)
+    valores = [valor_viejo[1],valor_viejo[2]]
+    inputs = []
+    opcion1 = validarOpcion("Ingresa 1 en caso de querer editar el nombre\nIngresa 2 para pasar al siguiente dato\n")
+    if(opcion1 == 1):
+        nombre = input("Escribe el nombre nuevo")
+        valores[0] = nombre
+        inputs.append(1)
+    else:
+        inputs.append(2)
+    opcion2 = validarOpcion("Ingresa 1 en caso de querer editar la direccion\nIngresa 2 para pasar al siguiente dato\n")
+    if(opcion2 == 1):
+        dir = input("Escribe la nueva direccion\n")
+        valores[1] = dir
+        inputs.append(1)
+    else:
+        inputs.append(2)
+    if(inputs[0] == 1 or inputs[1] == 1):
+        resultado = tienda.editar_tienda(id,valores)
+        if(resultado == True):
+            print("Se ha editado con exito la tienda")
+        else:
+            print("Ocurrio un error al editar la tienda")
+    else:
+        print("No hay nada que editar")
+def eliminar_tienda(id):
+    resultado = tienda.eliminar_tienda(id)
+    if(resultado == True):
+        print("Se ha eliminado la tienda con exito")
+    else:
+        print("Hubo un error al eliminar la tienda")    
+
 opcion = 0
-while(opcion!=3):
-    print("MENU\n1.- Crear Tienda\n2.- Mostrar Tiendas\n3.- Salir")
+while(opcion!=5):
+    print("MENU\n1.- Crear Tienda\n2.- Mostrar Tiendas\n3.- Editar Tienda\n4.- Eliminar Tienda\n5.- Salir")
     opcion = validarOpcion("Ingresa el numero que  corresponda a la opcion deseada\n")
     if(opcion==1):
         crear_tienda()
     elif(opcion == 2):
         mostrar_tiendas()
-    elif(opcion==3):
+    elif(opcion == 3):
+        mostrar_tiendas()
+        id = validarOpcion("Escribe el ID correspondiente de la tienda a editar\n")
+        editar_tienda(id)
+    elif(opcion == 4):
+        mostrar_tiendas()
+        id = validarOpcion("Escribe el ID correspondiente de la tienda a eliminar\n")
+        eliminar_tienda(id)
+    elif(opcion==5):
         print("Saliendo del programa...")
     else:
         print("Esa opcion no esta registrada. Saliendo del programa...")
