f6dec2cd5708fc59547795eb9d1b45259323a526
Victor Avila <victoravila@Mac-mini-de-Victor.local>
2023-05-23 11:30:12 -0600

    CRUD almacen completo

--- models/model_almacen.py
+++ models/model_almacen.py
@@ -11,11 +11,13 @@ def leer_almacenes():
     resultado = q.consulta_lista(resultado)
     return resultado
 def leer_almacen(id):
-    resultado = q.select("Select * from almacen where id ="+str(id)+"AND status=1")
+    consulta = "Select * from almacen where id="+str(id)+" AND estatus=1"
+    resultado = q.select(consulta)
     resultado = q.consulta_lista(resultado)
     return resultado[0]
 def editar_almacen(id,valores):
     #esto sirve para agregar el id al final de la lista ya que asi es la sintaxis para agregar valores a la consulta
+    valores.append(q.timestamp())
     valores.append(id)
     resultado = q.mod("Update almacen set nombre = %s, direccion = %s, updated_at = %s where id = %s",(valores[0],valores[1],valores[2],valores[3]))
     return resultado
