f34a3134403dec772bf8f2e23b932a124c67b45b
Victor Avila <victoravila@Mac-mini-de-Victor.local>
2023-05-29 13:56:13 -0600

    Agregando Practica Tkinter

---
+++ models/model_operaciones.py
@@ -0,0 +1,41 @@
+#Aqui se encuentran las operaciones de Entrada de Almacen, Salida de Almacen, Entrada en Tienda Y Venta
+import funciones_model as q
+
+def entrada_almacen(valores):
+    valores.append(q.timestamp())
+    resultado = q.mod("INSERT INTO empleado (cuenta_id, almacen_id, nombre, apellido_paterno, apellido_materno, estatus, created_at) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                  (valores[0], valores[1],valores[2],valores[3],valores[4],valores[5],valores[6]))
+    return resultado
+def crear_empleado_tienda(valores):
+    valores.append(q.timestamp())
+    resultado = q.mod("INSERT INTO empleado (cuenta_id, tienda_id, nombre, apellido_paterno, apellido_materno, estatus, created_at) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                  (valores[0], valores[1],valores[2],valores[3],valores[4],valores[5],valores[6]))
+    return resultado
+def leer_empleados():
+    resultado = q.select("SELECT empleado.id AS id, cuenta.correo AS cuenta, COALESCE(tienda.nombre, almacen.nombre) AS trabajo, empleado.nombre AS nombre, empleado.apellido_paterno AS apellido_paterno, empleado.apellido_materno AS apellido_materno, empleado.created_at AS created_at FROM empleado JOIN cuenta ON empleado.cuenta_id = cuenta.id LEFT JOIN tienda ON empleado.tienda_id = tienda.id LEFT JOIN almacen ON empleado.almacen_id = almacen.id WHERE empleado.estatus = 1")
+    resultado = q.consulta_lista(resultado)
+    return resultado
+def leer_empleado(id):
+    consulta = "SELECT empleado.id AS id, COALESCE(empleado.almacen_id,empleado.tienda_id) as id_trabajo, cuenta.correo AS cuenta, COALESCE(tienda.nombre, almacen.nombre) AS trabajo, empleado.nombre AS nombre, empleado.apellido_paterno AS apellido_paterno, empleado.apellido_materno AS apellido_materno, empleado.created_at AS created_at,empleado.cuenta_id as id_cuenta FROM empleado JOIN cuenta ON empleado.cuenta_id = cuenta.id LEFT JOIN tienda ON empleado.tienda_id = tienda.id LEFT JOIN almacen ON empleado.almacen_id = almacen.id WHERE empleado.estatus = 1 AND empleado.id ="+str(id)
+    resultado = q.select(consulta)
+    resultado = q.consulta_lista(resultado)
+    if(resultado != []):
+        return resultado[0]
+    return resultado
+def editar_empleado_almacen(id,valores):
+    #esto sirve para agregar el id al final de la lista ya que asi es la sintaxis para agregar valores a la consulta
+    valores.append(q.timestamp())
+    valores.append(id)
+    resultado = q.mod("Update empleado set almacen_id = %s, tienda_id = null, nombre = %s, apellido_paterno = %s, apellido_materno = %s, updated_at = %s where id = %s",(valores[0],valores[1],valores[2],valores[3],valores[4],valores[5]))
+    return resultado
+
+def editar_empleado_tienda(id,valores):
+    #esto sirve para agregar el id al final de la lista ya que asi es la sintaxis para agregar valores a la consulta
+    valores.append(q.timestamp())
+    valores.append(id)
+    resultado = q.mod("Update empleado set almacen_id = null, tienda_id = %s, nombre = %s, apellido_paterno = %s, apellido_materno = %s, updated_at = %s where id = %s",(valores[0],valores[1],valores[2],valores[3],valores[4],valores[5]))
+    return resultado
+
+def eliminar_empleado(id):
+    resultado = q.mod("Update empleado set estatus = %s where id = %s",(2,id))
+    return resultado
--- views/prueba.py
+++ views/prueba.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-# Crear una instancia de la ventana principal
+"""# Crear una instancia de la ventana principal
 ventana = tk.Tk()
 
 # Configurar propiedades de la ventana
@@ -15,4 +15,124 @@ boton = tk.Button(ventana, text="Haz clic")
 boton.pack()
 
 # Iniciar el bucle de eventos de la ventana
-ventana.mainloop()
+ventana.mainloop()"""
+import tkinter as tk
+import sys
+import os
+directorio_actual = os.getcwd()
+directorio_models = os.path.dirname(directorio_actual)
+directorio_models+= "/models"
+sys.path.append(directorio_models)
+import model_material as material
+import model_tipo_unidad as tipo_unidad
+def mostrar_materiales():
+    materiales = material.leer_materiales()
+    print("ID\tNombre\tPrecio\tTipo de Unidad\tFecha\n")
+    for row in materiales:
+        unidad = tipo_unidad.leer_tipo_unidad(row[1])
+        print(str(row[0])+"\t"+str(row[2])+"\t"+str(row[3])+"\t"+str(unidad[1])+"\t"+str(row[5]))
+"""import tkinter as tk
+
+def seleccionar():
+    seleccion = menu_var.get()
+    id_seleccion = None
+    for producto in productos:
+        if producto[1] == seleccion:
+            id_seleccion = producto[0]
+            break
+    print(id_seleccion)
+
+# Crear la ventana principal
+ventana = tk.Tk()
+ventana.title("Menu")
+ventana.geometry("400x300")
+# Lista de productos [ID, Nombre]
+productos = [[1, "Coca Cola"], [2, "Fanta"], [3, "Sprite"]]
+
+# Obtener los nombres de los productos
+nombres_productos = [producto[1] for producto in productos]
+
+# Variable para almacenar la selección
+menu_var = tk.StringVar(ventana)
+menu_var.set(nombres_productos[0])
+
+# Crear el menú desplegable
+menu = tk.OptionMenu(ventana, menu_var, *nombres_productos)
+menu.pack()
+
+# Crear el botón "Seleccionar"
+boton = tk.Button(ventana, text="Seleccionar", command=seleccionar)
+boton.pack()
+
+# Iniciar el bucle principal de la ventana
+ventana.mainloop()"""
+"""import tkinter as tk
+
+def cambiar_a_ventana_personalizada():
+    frame_principal.grid_remove()
+    frame_ventana_personalizada.grid()
+def volver_a_ventana_principal():
+    frame_ventana_personalizada.grid_remove()
+    frame_principal.grid()
+
+# Crear la ventana principal
+ventana_principal = tk.Tk()
+
+# Crear el frame de la ventana principal
+frame_principal = tk.Frame(ventana_principal)
+frame_principal.grid(row=0, column=0)
+
+# Crear el botón "Ventana Personalizada"
+btn_ventana_personalizada = tk.Button(frame_principal, text="Ventana Personalizada", command=cambiar_a_ventana_personalizada)
+btn_ventana_personalizada.pack()
+
+# Crear el frame de la ventana personalizada
+frame_ventana_personalizada = tk.Frame(ventana_principal)
+label_ventana_personalizada = tk.Label(frame_ventana_personalizada, text="Estás en otra ventana")
+btn_ventana_personalizada = tk.Button(frame_ventana_personalizada,text="Regresar",command=volver_a_ventana_principal)
+btn_ventana_personalizada.pack()
+label_ventana_personalizada.pack()
+
+# Mostrar el frame principal inicialmente
+frame_principal.grid()
+
+# Iniciar el bucle principal de la ventana principal
+ventana_principal.mainloop()"""
+
+
+"""import tkinter as tk
+
+def iniciar_sesion():
+    usuario = var_usuario.get()
+    contrasena = var_contrasena.get()
+    print("Usuario:", usuario)
+    print("Contraseña:", contrasena)
+
+ventana = tk.Tk()
+
+var_usuario = tk.StringVar()
+var_contrasena = tk.StringVar()
+
+label_usuario = tk.Label(ventana, text="Usuario:")
+label_usuario.pack()
+
+entry_usuario = tk.Entry(ventana, textvariable=var_usuario)
+entry_usuario.pack()
+
+label_contrasena = tk.Label(ventana, text="Contraseña:")
+label_contrasena.pack()
+
+entry_contrasena = tk.Entry(ventana, show="*", textvariable=var_contrasena)
+entry_contrasena.pack()
+
+btn_iniciar_sesion = tk.Button(ventana, text="Iniciar Sesión", command=iniciar_sesion)
+btn_iniciar_sesion.pack()
+
+ventana.mainloop()"""
+
+
+
+
+
+
+
